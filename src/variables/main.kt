2dad76004d750db88f4d915fffa0fecf88c4ada3
abdelrahmanmubarak <abdelrahmanmubarak123@gmail.com>
2024-03-07 10:49:21 +0200

    updates 1

--- src/variables/main.kt
+++ src/variables/main.kt
@@ -2,6 +2,10 @@ package variables
 
 
 fun main() {
+    val firstName2 = "ahmed"
+   // firstName2 = "mooaz"
+
+
     var firstName = "Mohamed"
     println(firstName.isEmpty())
     var lastName = ""
